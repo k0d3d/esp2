f6ed7382f2f8a734186c695025bea1f037b0314c
g0f0rky0s3lf <michael@i-x.it>
2017-04-07 13:12:39 +0000

    reviewing tests

--- src/app/activity/activity.service.spec.ts
+++ src/app/activity/activity.service.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed, inject } from '@angular/core/testing';
 
-import { ActivtiesService } from './activties.service';
+import { ActivtiesService } from './activty.service';
 
 describe('ActivtiesService', () => {
   beforeEach(() => {
--- src/app/location/location.component.spec.ts
+++ src/app/location/location.component.spec.ts
@@ -1,6 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { LocationsComponent } from './locations.component';
+import { LocationsComponent } from './location.component';
 
 describe('LocationsComponent', () => {
   let component: LocationsComponent;
